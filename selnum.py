52fb46dc904fb0e873286ec5d953bd59e8ba167e
Bhavesh Kumar <bhaveshkumar2806@gmail.com>
2021-08-31 09:20:40 +0530

    Added new lines

--- selnum.py
+++ selnum.py
@@ -24,7 +24,7 @@ def Captcha_Bypass(driver):
           EC.presence_of_element_located((By.CLASS_NAME, "main__part1"))
      )
 
-     cl = random.choice(['geetest_dot', 'geetest_small', 'geetest_scan', 'geetest_ring'])
+     cl = random.choice(['geetest_dot', 'geetest_small', 'geetest_bg', 'geetest_hook', 'geetest_h'])
      print(cl)
      captcha = WebDriverWait(driver, 15).until(
           EC.presence_of_element_located((By.CLASS_NAME, cl))
@@ -55,26 +55,46 @@ time.sleep(10)
 all_links = driver.find_elements_by_class_name('result-list-entry__brand-logo-container')
 for link in all_links:
      link.click()
-     time.sleep(10)
+
+     modal_popup = WebDriverWait(driver, 15).until(
+          EC.presence_of_element_located((By.XPATH, '//*[@id="is24-expose-modal"]/div/div/div/div/div/div[2]/button'))
+     )
+     modal_popup.click()
+
+     time.sleep(5)
+
+     title = driver.find_element_by_xpath('//*[@id="expose-title"]')
      multiple_elements = driver.find_elements_by_class_name('breadcrumb__link')
      choose_states = []
      for i in multiple_elements:
           choose_states.append(i.text)
      
      state = choose_states[1]
-     scout_id = driver.find_element_by_class_name('is24-scoutid__content padding-top-s').text
+     scout_id = driver.find_element_by_xpath('//*[@id="is24-content"]/div[2]/div/div[3]/div/div/div').text
 
      address = driver.find_element_by_class_name('zip-region-and-country').text
 
-     grids = []
-     grid_values = driver.find_elements_by_class_name('grid')
-     
+     grids = {}
+     grid_values = driver.find_elements_by_xpath('#//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[1]')
+     for grid in grid_values:
+          dt = grid.find_element_by_xpath('//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[1]/dt').text
+          dd = grid.find_element_by_xpath('//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[1]/dd').text
+          if dt is not None:
+               grids[dt] = dd
 
-     print(multiple_elements)
+
+     print(title)
      print(state)
+     print(scout_id)
+     print(address)
+     print(grids)
      break
 
 
+#//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[1]/dt
+#//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[2]/dt
+
+#//*[@id="is24-content"]/div[3]/div[1]/div[2]/dl[1]
 
 # //*[@id="result-xl-129280384"]/div[1]/a[1]
 # //*[@id="result-xl-128580468"]/div[1]/a[1]
